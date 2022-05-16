11efc1f93bd5eb016119a9dc753ed86176808721
nikoDev <nikolas090189@gmail.com>
2022-05-16 08:51:28 -0300

    Login modificado

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,12 +1,11 @@
 import LockOpenIcon from '@mui/icons-material/LockOpen'
 import { Avatar, Button, Grid, Paper, TextField } from '@mui/material'
-import { Box } from '@mui/system'
 
 export const Login = () => {
-    const boxStyle = {
-        marginTop: '2em',
+    const paperStyle = {
+        marginTop: '3em',
         padding: 20,
-        height: '70vh',
+        height: '28em',
         width: 300,
         margin: '20px auto',
         borderRadius: '15px 15px 15px 15px',
@@ -15,17 +14,18 @@ export const Login = () => {
 
     return (
         <Grid display="flex" flexDirection="column">
-            <Paper elevation={10} style={boxStyle}>
+            <Paper elevation={10} style={paperStyle}>
                 <Grid display="flex" justifyContent="center" alignItems="center" flexDirection="column">
                     <Avatar sx={{ backgroundColor: '#2ea3f2', boxShadow: '3px 3px 4px  rgba(60,60,60,0.5)' }}>
                         <LockOpenIcon />
                     </Avatar>
                     <h3>Ingreso a declaración jurada</h3>
                 </Grid>
+
                 <TextField
                     sx={{
                         marginTop: '3em',
-                        marginBottom: '1em',
+                        marginBottom: '3em',
                     }}
                     label="CUIT/CUIL"
                     type="number"
@@ -36,8 +36,7 @@ export const Login = () => {
                 />
                 <TextField
                     sx={{
-                        marginTop: '1em',
-                        marginBottom: '4em',
+                        marginBottom: '6em',
                     }}
                     label="Contraseña"
                     placeholder="Ingrese su contraseña"
